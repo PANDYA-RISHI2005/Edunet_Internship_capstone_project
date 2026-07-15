9c1151e549cbd002875342b111442e1d3ff43edf
PANDYA RISHI KANAIYALAL <pandyarishi93@gmail.com>
2026-07-15 23:58:14 +0530

    Add files via upload

---
+++ Python_code-file/utils.py
@@ -0,0 +1,75 @@
+waste_info = {
+
+"battery":{
+"category":"E-Waste",
+"bin":"E-Waste Collection Center",
+"tip":"Never throw batteries in household waste."
+},
+
+"biological":{
+"category":"Organic",
+"bin":"Green Bin",
+"tip":"Can be composted."
+},
+
+"paper":{
+"category":"Paper",
+"bin":"Blue Bin",
+"tip":"Recycle clean paper."
+},
+
+"cardboard":{
+"category":"Paper",
+"bin":"Blue Bin",
+"tip":"Flatten cardboard boxes before recycling."
+},
+
+"plastic":{
+"category":"Plastic",
+"bin":"Yellow Bin",
+"tip":"Wash plastic before recycling."
+},
+
+"metal":{
+"category":"Metal",
+"bin":"Metal Recycling Bin",
+"tip":"Recycle aluminum and steel products."
+},
+
+"white-glass":{
+"category":"Glass",
+"bin":"Glass Recycling Bin",
+"tip": "Recycle clear glass bottles and jars after cleaning them."
+},
+
+"green-glass":{
+"category":"Glass",
+"bin":"Glass Recycling Bin",
+"tip":"Place green glass bottles in the glass recycling bin."
+},
+
+"brown-glass":{
+"category":"Glass",
+"bin":"Glass Recycling Bin",
+"tip":"Recycle brown glass bottles separately if your local recycling center requires it."
+},
+
+"clothes":{
+"category":"Textile",
+"bin":"Cloth Donation Center",
+"tip":"Donate usable clothes."
+},
+
+"shoes":{
+"category":"Textile",
+"bin":"Donation Center",
+"tip":"Repair or donate old shoes."
+},
+
+"trash":{
+"category":"Other Waste",
+"bin":"Black Bin",
+"tip":"Dispose responsibly and reduce waste."
+}
+
+}
